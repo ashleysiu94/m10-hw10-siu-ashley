010aac850de63d80c29e6adab33bd41e7183f957
Ashley Siu <ashley.c.siu@gmail.com>
2020-11-05 23:22:11 -0800

    Print 212 in boiling-tem w arrow function

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // It is always helpful to use comments in your code!
+
+//The number “212” immediately prints inside of the span with the ID "boiling-temp" using an arrow function
+const boilingTemp = () => {
+  document.getElementById("boiling-temp").innerHTML = 212;
+};
+boilingTemp();
