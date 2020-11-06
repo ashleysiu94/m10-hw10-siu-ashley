7839226bb79f5bf8262bc5218ba485cd4c31d647
Ashley Siu <ashley.c.siu@gmail.com>
2020-11-05 23:25:01 -0800

    Use setTimeout to show statement when above boiling point

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ freezingTemp();
 //Any number of your choice immediately prints inside of the span with the ID "water-temp" using an arrow function
 let waterTemp = 250
 document.getElementById("water-temp").innerHTML = waterTemp;
+
+//Use a setTimeout() method to make the following occur after 3 seconds (3000 milliseconds):
+
+//If the number stored in “water-temp” is above 212, remove the class “hide” and add the class “show” to the section with the ID “boiling” so that the message “The water is hot. I think it is boiling.” appears
+setTimeout ( function aboveBoilingPoint() {
+  if (waterTemp > 212) {
+     document.getElementById("boiling").classList.remove("hide");
+     document.getElementById("boiling").classList.add("show");
+  }
+}, 3000)
