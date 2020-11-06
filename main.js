6c142ab53f289622083dcfaa97aa883e01ff5745
Ashley Siu <ashley.c.siu@gmail.com>
2020-11-05 23:36:41 -0800

    Use setTimeout to show statement when below freezing point

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const freezingTemp = () => {
 freezingTemp();
 
 //Any number of your choice immediately prints inside of the span with the ID "water-temp" using an arrow function
-let waterTemp = 250
+let waterTemp = 310
 document.getElementById("water-temp").innerHTML = waterTemp;
 
 //Use a setTimeout() method to make the following occur after 3 seconds (3000 milliseconds):
@@ -23,5 +23,13 @@ setTimeout ( function aboveBoilingPoint() {
   if (waterTemp > 212) {
      document.getElementById("boiling").classList.remove("hide");
      document.getElementById("boiling").classList.add("show");
+  }
+}, 3000)
+
+//If the number stored in “water-temp” is below 32, remove the class “hide” and add the class “show” to the section with the ID “frozen” so that the message “The water is cold. I think it is frozen.” appears
+setTimeout ( function belowFreezingPoint() {
+  if (waterTemp < 32) {
+     document.getElementById("frozen").classList.remove("hide");
+     document.getElementById("frozen").classList.add("show");
   }
 }, 3000)
