de916c8f5f671069cfe81e5877c7a8a7e777d9cd
Ashley Siu <ashley.c.siu@gmail.com>
2020-11-06 20:34:17 -0800

    Fix statement reveals

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const freezingTemp = () => {
 freezingTemp();
 
 //Any number of your choice immediately prints inside of the span with the ID "water-temp" using an arrow function
-let waterTemp = 250
+let waterTemp = 270
 document.getElementById("water-temp").innerHTML = waterTemp;
 
 //Use a setTimeout() method to make the following occur after 3 seconds (3000 milliseconds):
@@ -36,10 +36,8 @@ setTimeout ( function belowFreezingPoint() {
 
 //If the number stored in “water-temp” is neither above 212 nor below 32, remove the class “hide” and add the class “show” to the section with the ID “good-temp” so that the message “The water is fine. Jump on in.” appears
 setTimeout ( function waterGoodTemp() {
-  if (32 <= waterTemp <= 212) {
+  if (32 <= waterTemp && waterTemp <= 212) {
      document.getElementById("good-temp").classList.remove("hide");
      document.getElementById("good-temp").classList.add("show");
-  } else {
-     document.getElementsByClassName("good-temp").classList.add("hide");
   }
 }, 3000)
